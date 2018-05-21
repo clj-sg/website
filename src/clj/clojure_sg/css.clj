cd04d34fe088f9b38d3817e5a9b5f611d87cb9df
rohit <rohitangsudas91@gmail.com>
2018-05-21 18:13:40 +0800

    Completed the css file, however the testing remains

--- src/clj/clojure_sg/css.clj
+++ src/clj/clojure_sg/css.clj
@@ -75,14 +75,6 @@
     []
       (g/css [:a:hover {text-declaration "none"}]))
 
-
-;blockquote
-;{ margin: 20px 0;
-;   padding: 10px 20px 0 20px;
-;     border: 1px solid #E5E5DB;
-;       background: #FFF;}
-
-
 (defn blockquote
   []
   (g/css [:blockquote {:margin "20px 0" 
@@ -114,7 +106,7 @@
                  :width "auto"
                  :margin-right (u/px 10)}]))
 
-(defn right)
+(defn right
   []
   (g/css [:right {:float "left"
                  :width "auto"
@@ -143,6 +135,14 @@
                     :margin-bottom (u/px 20)
                     :border "10px solid #DDD"}]))
 
+(defn hash-logo
+  []
+  (g/css [:#logo {:width (u/px 880) 
+                  :position "relative"
+                  height: (u/px 140)
+                  background: "#F5F5F5"}]))
+
+
 (defn hash-logo-logo_text
   []
   (g/css [:#logo :#logo_text {:position "absolute"
@@ -156,12 +156,10 @@
                                     :margin "0 0 0 9px"}]))
 
 
-#logo_text h1 a .logo_colour
-{ color: #00C6F0;}
 
 (defn hash-logo-a-text-colour
   []
-  (g/css [:#logo_text [:h1 [:a [:.logo_color {:color "#00C6F0"}]]]))
+  (g/css [:#logo_text [:h1 [:a [:.logo_color {:color "#00C6F0"}]]]]))
 
 #logo_text a:hover .logo_colour
  { color: #DDD;}
@@ -171,8 +169,427 @@
   (g/css [:#logo_text [:a:hover [:.logo_colour {:color "#DDD"}]]]))
 
 
+(defn  hash-logo-text-h1-a-hover
+  []
+  (g/css [:#logo_text [:h1 [:a :a:hover {:padding "22px 0 0 0" 
+                                         :color "#323232"
+                                         :letter-spacing (u/em 0.1)
+                                         :text-decoration "none"}]]]))
 
 
+;;#logo_text h2
+;;{ font-size: 120%;
+;;   padding: 4px 0 0 0;
+;;     color: #999;}
+
+(defn hash-logo-text
+  []
+  (g/css [:#logo_text [:h2 {:font-size {u/percent 120}
+                            :padding "4px 0 0 0"
+                            :color: "#999"}]]))
+
+;;#menubar
+;;{ width: 872px;
+;;        height: 45px;
+;;          padding-right: 8px;
+;;            background: #DDD;
+;;              border-top: 1px solid #CCC;}
+
+(defn hash-menubar
+  []
+  (g/css [:#menubar {:width (u/px 872)
+                     :padding-right (u/px 45)
+                     :background "#DDD"
+                     :border-top "1px solid #CCC"}]))
+
+;;ul#menu
+;;{ float: right;
+;;   margin: 0;}
+(defn ul-hash-menu
+  []
+  (g/css [:ul#menu {:float "right"
+                    :margin 0}]))
+
+;;   ul#menu li
+;;   { float: left;
+;;      padding: 0 0 0 9px;
+;;        list-style: none;
+;;          margin: 8px 4px 0 4px;}
+(defn ul-hash-menu-li
+  []
+  (g/css [:ul#menu [:li {:float "left"
+                         :padding "0 0 0 9px"
+                         :list-style "none"
+                         :margin "8px 4px 0 4px"}]]))
+
+;;ul#menu li a
+;;{ font: normal 100% 'trebuchet ms', sans-serif;
+;;  display: block;
+;;  float: left;
+;;   height: 20px;
+;;  padding: 6px 20px 5px 20px;
+;;  text-align: center;
+;;  color: #FFF;
+;;  text-decoration: none;
+;;  background: #BBB;}
+
+(defn ul-hash-menu-li-a
+  []
+  (g/css [:ul#menu [:li [:a {:font "normal 100% 'trebuchet ms', sans-serif"
+                             :display "block"
+                             :float "left"
+                             :height (u/px 20)
+                             :padding "6px 20px 5px 20px"
+                             :text-align "center"
+                             :color "#FFF"
+                             :text-decoration "none"
+                             :background "#BBB"}]]]))
+
+;;ul#menu li.selected a
+;; { height: 20px;
+;;   padding: 6px 20px 5px 11px;}
+
+(defn ul-hash-menu-li-selected-a
+  []
+  (g/css [:ul#menu [:li.selected [:a {:height (u/px 20)
+                                      :padding "6px 20px 5px 11px"}]]]))
+
+  
+;;ul#menu li.selected
+;;{ margin: 8px 4px 0 13px;
+;;  background: #00C6F0;}
+
+(defn ul-hash-menu-li-selected
+  []
+  (g/css [:ul#menu [:li.selected {:margin "8px 4px 0 13px"
+                                  :background "#00C6F0"}]]))
 
 
+;;ul#menu li.selected a, ul#menu li.selected a:hover
+;;{ background: #00C6F0;
+;;  color: #FFF;}
+(defn ul-menu-li-selected-a-hover
+  []
+  (g/css [:ul#menu [:li.selected [:a :a:hover {:background "00C6F0"
+                                               :color "#FFF"}]]]))
+
+;;ul#menu li a:hover
+;;  { color: #323232;}
+(defn ul-menu-li-a-hover
+  []
+  (g/css [:ul#menu [:li :a:hover {:color "#323232"}]]))
+
+;;#site_content
+;;{ width: 880px;
+;;overflow: hidden;
+;;margin: 20px auto 0 auto;
+;;padding: 0 0 10px 0;}
+
+(defn hash-site-content
+  []
+  (g/css [:#site_content {:width (u/px 880)
+                          :overflow "hidden"
+                          :margin "20px auto 0 auto"
+                          padding "0 0 10px 0"}]))
+
+;;#sidebar_container
+;;{ float: left;
+;;   width: 224px;}
+(defn hash-sidebar-container
+  []
+  (g/css [:#sidebar_container {:float "left"
+                               :width (u/px 224)}]))
+
+;;   .sidebar_top
+;;   { width: 222px;
+;;      height: 14px;
+;;        background: transparent url(side_top.png) no-repeat;}
+(defn sidebar-top
+  []
+  (g/css [:sidebar_top {:width (u/px 224)
+                        :height (u/px 14)
+                        :background "transparent url(side_top.png) no-repeat"}]))
+
+;;        .sidebar_base
+;;        { width: 222px;
+;;           height: 14px;
+;;             background: url(side_base.png) no-repeat;}
+
+(defn sidebar-base
+    []
+      (g/css [:sidebar_base {:width (u/px 224)
+                             :height (u/px 14)
+                             :background "transparent url(side_bar.png) no-repeat"}]))
+
+;;.sidebar
+;;{ float: right;
+;;  width: 222px;
+;;  padding: 0;
+;;  margin: 0 0 16px 0;}
+
+(defn sidebar
+  []
+  (g/css {:float "right"
+          :width (u/px 222)
+          :padding 0
+          :margin "0 0 16px 0"}))
+
+;;  .sidebar_item
+;; { background: url(side_back.png) repeat-y;
+;;          padding: 0 15px;
+;;            width: 192px;}
+
+(defn sidebar-item
+  []
+  (g/css [:.siderbar_item {:background "url(side_back.png) repeat-y"
+                           :padding "0 15px"
+                           :width (u/px 192)}]))
+
+;;            .sidebar li a.selected
+;;            { color: #444;}
+(defn sidebar-li-a-selected
+  []
+  (g/css [:sidebar [:li :a.selected {:color "#444"}]]))
+
+
+;;             .sidebar ul
+;;             { margin: 0;}
+
+(defn sidebar-ul
+  []
+  (g/css [:sidebar [:ul {:margin 0}]]))
+
+;;              #content
+;;              { text-align: left;
+;;                 width: 620px;
+;;                   padding: 0 0 0 5px;
+;;                     float: right;}
+
+(defn content
+  []
+  (g/css [:content {:text-align "left"
+                    :width (u/px 620)
+                    :padding "0 0 0 5px"
+                    :float "right"}]))
+
+;;                     #content ul
+;;                     { margin: 2px 0 22px 0px;}
+(defn hash-content-ul
+  []
+  (g/css [:hash-content-ul {:margin "2px 0 22px 0px"}]))
+
+;;                      #content ul li, .sidebar ul li
+;;                      { list-style-type: none;
+;;                         background: url(bullet.png) no-repeat;
+;;                           margin: 0 0 0 0;
+;;                             padding: 0 0 4px 25px;
+;;                               line-height: 1.5em;}
+
+
+(defn hash-content-ul-li-sidebar-ul-li
+  []
+  (g/css [:#content :#sidebar [:ul [:li {:list-style-type "none"
+                                         :background "url(bullet.png) no-repeat"
+                                         :margin "0 0 0 0"
+                                         :padding "0 0 4px 25px"
+                                         :line-height (u/em 1.5)}]]]))
+
+
+;;#footer
+;;{ width: 100%;
+;;   font-family: 'trebuchet ms', sans-serif;
+;;     font-size: 100%;
+;;       height: 80px;
+;;         padding: 28px 0 5px 0;
+;;           text-align: center;
+;;             background: #CCC;
+;;               border-top: 1px solid #AAA;
+;;                 color: #FFF;}
+(defn hash-footer
+  []
+  (g/css [:#footer {:width (u/percent 100)
+                    :font-family "'trebuchet ms', sans-serif"
+                    :font-size (u/percent 100)
+                    :height (u/px 80)
+                    :padding "28px 0 5px 0"
+                    :text-align "center"
+                    :background "#CCC"
+                    :border-top "1px solid #AAA"
+                    :color "#FFF"}]))
+(defn hash-footer-p
+  []
+  (g/css [:#footer [:p {:line-height (u/em 1.7)
+                        :padding "0 0 10px 0" }]]))
+
+
+;;                 #footer p
+;;                 { line-height: 1.7em;
+;;                    padding: 0 0 10px 0;}
+
+
+;;                    #footer a
+;;                    { color: #FFF;
+;;                       text-decoration: none;}
+
+(defn  hash-footer-a
+  []
+  (g/css [:#footer [:a {:color "#FFF"
+                        text-decoration: "none"}]]))
+
+;;#footer a:hover
+;;{ color: #00C6F0;
+;;text-decoration: none;}
+
+(defn hash-footer-a-hover
+  []
+  (g/css [:#footer [:a:hover {:color "#00C6F0"
+                              :text-declaration "none"}]]))
+
+;;.search
+;;{ color: #5D5D5D;
+;;    border: 1px solid #BBB;
+;;    width: 134px;
+;;padding: 4px;
+;;font: 100% arial, sans-serif;}
+
+(defn dot-search
+  []
+  (g/css [:.search  {:color "#5D5D5D"
+                     :border "1px solid #BBB"
+                     :width (u/px 134)
+                     :padding (u/px 4)
+                     :font "100% arial, sans-seri"}]))
+
+;;.form_settings
+;;{ margin: 15px 0 0 0;}
+(defn dot-form-settings
+  []
+  (g/css [:.form_settings {:margin "15px 0 0 0"}]))
+
+;;.form_settings p
+;;{ padding: 0 0 4px 0;}
+(defn dot-from-settings-p
+  []
+  (g/css [:.form_settings {:padding "0 0 4px 0"}]))
+
+;;.form_settings span
+;;{ float: left;
+;;width: 200px;
+;;text-align: left;}
+
+(defn form-settings-span
+  []
+  (g/css [:.form_settings [:span {:float "left"
+                                  :width (u/px 200)
+                                  :text-align "left"}]]))
+
+;;form_settings input, .form_settings textarea
+;;{ padding: 5px;
+;;width: 299px;
+;;font: 100% arial;
+;;border: 1px solid #E5E5DB;
+;;background: #FFF;
+;;color: #47433F;}
+
+(defn form-settings-input-textarea
+  []
+  (g/css [:form_settings [:input :textarea {:padding (u/px 5)
+                                            :width (u/px 299)
+                                            :font "100% arial"
+                                            :border "1px solid #E5E5D8"
+                                            :background "#FFF"
+                                            :color "#47433F"}]]))
+
+;;.form_settings .submit
+;;{ font: 100% arial;
+;;   border: 0;
+;;     width: 99px;
+;;       margin: 0 0 0 212px;
+;;         height: 33px;
+;;           padding: 2px 0 3px 0;
+;;             cursor: pointer;
+;;               background: #3B3B3B;
+;;                 color: #FFF;}
+(defn dot-form-settings-dot-submit
+  []
+  (g/css [:.form_settings [:.submit {:border 0
+                                     :width (u/px 99)
+                                     :margin "0 0 0 212px"
+                                     :height (u/px 33)
+                                     :padding "2px 0 3px 0"
+                                     :cursor "pointer"
+                                     :background "#3B3B3B"
+                                     :color "#FFF"}]]))
+
+;;                 .form_settings textarea, .form_settings select
+;;                 { font: 100% arial;
+;;                    width: 299px;}
+
+(defn dot-form-settings-textarea-select
+  []
+  (g/css [:.form_settings [:textarea :select {:font "100% arial"
+                                              :width (u/px 299)}]]))
+
+;;                    .form_settings select
+;;                    { width: 310px;}
+(defn dot-form-settings-select
+  []
+  (g/css [:.form_settings [:select {:width (u/px 310)}]]))
+
+;;   .form_settings .checkbox
+;;   { margin: 4px 0;
+;;     padding: 0;
+;;     width: 14px;
+;;     border: 0;
+;;     background: none;}
+(defn  dot-form-settings-checkbox
+  []
+  (g/css [:.form_settings [:.checkbox {:margin "4px 0"
+                                       :padding "0"
+                                       :width (u/px 14)
+                                       :border 0
+                                       :background "none"}]]))
+
+#    .separator
+#    { width: 100%;
+#      height: 0;
+#      border-top: 1px solid #D9D5CF;
+#      border-bottom: 1px solid #FFF;
+#      margin: 0 0 20px 0;}
+
+(defn separator
+      []
+      (g/css [:.separator {:width (u/px 100)
+                           :height 0
+                           :border-top "1px slid #D9D5CF"
+                           :border-bottom "1px solid #FFF"
+                           :margin "0 0 20px 0"}]))
+
+;;   table
+;;  { margin: 10px 0 30px 0;}
+(defn table
+      []
+      (g/css [:table {:margin "10px 0 30px 0"}]))
+
+;;  table tr th, table tr td
+;; { background: #3B3B3B;
+;;color: #FFF;
+;;padding: 7px 4px;
+;;text-align: left;}
+(defn table-tr-th-td
+      []
+      (g/css [:table [:tr [:th :td {:background "#3B3B3B"
+                                    :color "#FFF"
+                                    :padding "7px 4px"
+                                    :text-align "left"}]]]))
+
+;;table tr td
+;;{  background: #E5E5DB;
+;;   color: #47433F;
+;;   border-top: 1px solid #FFF;}
+(defn table-tr-td
+      []
+      (g/css [:table [:tr [:td {:background "#E5E5D8"
+                                :color "#47433F"
+                                :border-top "1px solid #FFF"}]]]))
 
