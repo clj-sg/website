d443c504b0dd444695ca641672087e2d5c281ccd
rohit <rohitangsudas91@gmail.com>
2018-05-22 16:00:39 +0800

    added the validation function

--- src/cljs/clojure_sg/login.cljs
+++ src/cljs/clojure_sg/login.cljs
@@ -1,19 +1,49 @@
-(ns clojure-sg.login
-  (:require [domina.core :refer [by-id value]]))
+(ns modern-cljs.login
+    (:require [domina.core :refer [append!
+                                   by-class
+                                   by-id
+                                   destroy!
+                                   prepend!
+                                   value
+                                   attr]]
+                          [domina.events :refer [listen! prevent-default]]
+                          [hiccups.runtime]
+                          [modern-cljs.login.validators :refer [user-credential-errors]])
+      (:require-macros [hiccups.core :refer [html]]))
 
-(defn validate-form []
-  (if (and (> (count (value (by-id "email"))) 0)
-           (> (count (value (by-id "password"))) 0))
-    true
-    (do (js/alert "Please, complete the form!")
-        false)))
+(defn validate-email [email]
+    (destroy! (by-class "email"))
+      (if-let [errors (:email (user-credential-errors (value email) nil))]
+      (do
+         (prepend! (by-id "loginForm") (html [:div.help.email (first errors)]))
+         false)
+      true))
+
+(defn validate-password [password]
+    (destroy! (by-class "password"))
+     (if-let [errors (:password (user-credential-errors nil (value password)))]
+            (do
+                 (append! (by-id "loginForm") (html [:div.help.password (first errors)]))
+                 false)
+            true))
+
+(defn validate-form [evt email password]
+    (if-let [{e-errs :email p-errs :password} (user-credential-errors (value email) (value password))]
+          (if (or e-errs p-errs)
+                  (do
+                              (destroy! (by-class "help"))
+                                      (prevent-default evt)
+                                              (append! (by-id "loginForm") (html [:div.help "Please complete the form."])))
+                        (prevent-default evt))
+              true))
 
 (defn ^:export init []
-  ;; verify that js/document exists and that it has a getElementById
-  ;; property
-  (if (and js/document
-           (.-getElementById js/document))
-    ;; get loginForm by element id and set its onsubmit property to
-    ;; our validate-form function
-    (let [login-form (by-id "loginForm")]
-      (set! (.-onsubmit login-form) validate-form))))
+    (if (and js/document
+                        (aget js/document "getElementById"))
+          (let [email (by-id "email")
+                          password (by-id "password")]
+                  (listen! (by-id "submit") :click (fn [evt] (validate-form evt email password)))
+                        (listen! email :blur (fn [evt] (validate-email email)))
+                              (listen! password :blur (fn [evt] (validate-password password))))))
+
+
